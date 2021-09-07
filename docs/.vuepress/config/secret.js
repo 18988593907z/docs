98fb2bbfa1ecb118ac82cb556fdfab6e6a31b44c
zhang <1528358840@qq.com>
2021-09-07 15:49:39 +0800

    feat: Travis CI

---
+++ docs/.vuepress/config/secret.js
@@ -0,0 +1,9 @@
+module.exports = {
+    //google analytics
+    clientId: 'f13039e164905bb473e9',
+    clientSecret: '705104717e0263adea9a3a5dbdf172c0c5ebd85c',
+    g: 'G-34WQZDSH1L',
+    //leancloud-valine-comment
+    appId: 'bBflOchfJXySHs7oxfVMaVhL-gzGzoHsz',
+    appKey: 'E20DGz1IfKSaDRFxXQSlOkTm'
+}
